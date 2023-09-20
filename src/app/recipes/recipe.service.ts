c9332fff287b85f4156332fb8996a844bb995347
KavinKandasamy <kavinkandasamy3147@gmail.com>
2023-09-20 15:06:28 +0530

    Routing

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -34,7 +34,7 @@ export class RecipeService {
     ),
   ];
 
-  getRecipe() {
+  getRecipes() {
     return this.recipes.slice();
   }
 
@@ -42,5 +42,8 @@ export class RecipeService {
     this.slService.addIngredients(ingredients)
   }
 
+  getRecipe(index: number){
+    return this.recipes[index]
+  }
   constructor(private slService: ShoppingListService){}
 }
